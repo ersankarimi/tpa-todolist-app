0890ce94c2b621e4b85fcd9fbae4a04e7c5ab5f7
ersankarimi <ersankarimii311@gmail.com>
2022-11-05 02:58:45 +0800

    feat: configured router for the app
    Setup the router for the app using `react-router-dom`

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
