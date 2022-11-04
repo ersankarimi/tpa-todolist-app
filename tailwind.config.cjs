0e352d90951e9bc03a17e1b0c682fd1760f9fc89
ersankarimi <ersankarimii311@gmail.com>
2022-11-05 03:00:12 +0800

    feat: configured screens and extend property
    Add new values for screens property and extends the background image and font family default style from Tailwind CSS.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      backgroundImage: {
+        "main-dark": "url('src/assets/images/hero-dark@90.dba36cdf.jpg')",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+    },
   },
-  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+
   plugins: [],
 };
