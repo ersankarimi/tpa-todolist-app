63d3abd29c2e76d8eaed8ab8a13348b04d33766c
ersankarimi <ersankarimii311@gmail.com>
2022-11-04 16:01:33 +0800

    Setup boilerplate

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+  plugins: [],
+};
