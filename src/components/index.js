4232d7adc1ad4ffac98c407af5d6574ea877556b
ersankarimi <ersankarimii311@gmail.com>
2022-11-05 21:10:52 +0800

    feat: export the EmptyTodo component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Badge from "./Badge";
+import EmptyTodo from "./EmptyTodo";
 import FormInputTodo from "./FormInputTodo";
 import TodoItem from "./TodoItem";
 
-export { Badge, FormInputTodo, TodoItem };
+export { Badge, EmptyTodo, FormInputTodo, TodoItem };
