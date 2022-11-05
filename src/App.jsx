11019c13f66057384607750d8e7507946bcd0ec9
ersankarimi <ersankarimii311@gmail.com>
2022-11-05 14:27:34 +0800

    feat: changes classes style and add LayoutGroup for badge
    Update some class's style names and wrapped all badges with LayoutGroup for group motion components that should perform layout animations together.
    
    Created categoryOnHover to set the state of current hovering the badge .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { LayoutGroup, motion } from "framer-motion";
 
 import { Badge, FormInputTodo } from "./components";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [categoryOnHover, setCategoryOnHover] = useState("null");
   const categories = ["All", "Active", "Completed"];
 
   return (
@@ -14,17 +15,18 @@ function App() {
       </h1>
       <section className="w-full">
         <FormInputTodo>
-          <form className="flex w-full flex-col items-center justify-around gap-8 py-4 md:flex-row">
+          <form className="flex w-full flex-col items-center justify-around gap-8 py-4 sm:m-auto sm:w-10/12 md:flex-row lg:w-2/3 xl:w-1/2 2xl:w-2/5">
             <input
               type="text"
-              className="w-full appearance-none rounded-md bg-slate-800/20 py-3 px-4 text-slate-100 ring-1 ring-slate-300/10 backdrop-blur-md placeholder:text-sm placeholder:text-slate-400/50 focus:outline-none focus:ring-1 focus:ring-slate-300/40"
+              className="w-full appearance-none rounded-md bg-slate-800/20 py-3 px-4 text-slate-100 ring-1 ring-slate-300/10 backdrop-blur-md placeholder:text-sm placeholder:text-slate-400/80 focus:outline-none focus:ring-1 focus:ring-slate-300/40"
               name="add-todo"
               id="add-todo"
               placeholder="What to do ?"
+              autoComplete="off"
             />
             <motion.button
               type="submit"
-              className="w-1/2 rounded-lg bg-slate-700/80 py-2 px-3 text-slate-100/90 shadow-2xl ring-1 ring-slate-900/10 backdrop-blur-sm hover:ring-slate-300/20 focus:outline-none focus:ring-1 focus:ring-slate-300/40"
+              className="w-1/2 rounded-lg bg-slate-700/80 py-3 px-4 text-slate-100/90 shadow-2xl ring-1 ring-slate-900/10 backdrop-blur-sm hover:ring-slate-300/20 focus:outline-none focus:ring-1 focus:ring-slate-300/40 lg:w-1/3 lg:px-4"
               whileTap={{
                 scale: 0.925,
                 transition: {
@@ -38,12 +40,23 @@ function App() {
         </FormInputTodo>
       </section>
 
-      <section className="flex items-center justify-center gap-4">
-        {categories.map((category) => (
-          <Badge key={category} isActive={selectedCategory === category}>
-            {category}
-          </Badge>
-        ))}
+      <section
+        className="flex items-center justify-center gap-4 lg:gap-8"
+        onMouseLeave={() => setCategoryOnHover("null")}
+      >
+        <LayoutGroup id="badge-group">
+          {categories.map((category) => (
+            <Badge
+              key={category}
+              setSelectedCategory={setSelectedCategory}
+              setCategoryOnHover={setCategoryOnHover}
+              onHover={categoryOnHover === category}
+              isActive={selectedCategory === category}
+            >
+              {category}
+            </Badge>
+          ))}
+        </LayoutGroup>
       </section>
     </main>
   );
