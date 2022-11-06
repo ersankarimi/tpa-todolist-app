87ba8ab529945ae6fabdcbc33c60a42377dfa1ba
ersankarimi <ersankarimii311@gmail.com>
2022-11-07 01:33:17 +0800

    feat: add a new reducer
    Add a modalDialogReducer to the store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import modalDialogReducer from "./features/modal/modalSlice";
 import todoReducer from "./features/todo/todoSlice";
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
+    modalDialog: modalDialogReducer,
   },
 });
 
