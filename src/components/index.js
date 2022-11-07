79c9f85697adc375ba747be34ba086c7ffdd8e22
ersankarimi <ersankarimii311@gmail.com>
2022-11-07 12:00:42 +0800

    refactor: changes the Badge and TodoItem import export

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import Badge from "./Badge";
+import CategoryButton from "./ CategoryButton";
 import EmptyTodo from "./EmptyTodo";
 import FormInputTodo from "./FormInputTodo";
+import ListTodos from "./ListTodos";
 import ModalDialog from "./ModalDialog";
-import TodoItem from "./TodoItem";
 
-export { Badge, EmptyTodo, FormInputTodo, ModalDialog, TodoItem };
+export { CategoryButton, EmptyTodo, FormInputTodo, ListTodos, ModalDialog };
