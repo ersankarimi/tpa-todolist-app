b37563f5bcc30e66cb19152ee68bd137d410a5cf
ersankarimi <ersankarimii311@gmail.com>
2022-11-07 12:00:10 +0800

    feat: allow user to filter todos by selected category
    Import useMemo , useEffect, and useSearchParams hooks.
    
    Change import the Badge to the CategoryButton and the TodoItem to the ListTodos.
    
    Initialize searchParams, setSearchParams using useSearchParams hook to get the `filter` query parameter value.
    
    Initialize filterTodoByCategory using useMemo hook. The value of this variable will changes and updated if the `filter` query params value changed (the dependency). This value will passed to the ListTodos component as filterTodoByCategory prop. And ListTodos will render todo item based selected category.
    
    Defined useEffect to check the `filter` query params value. If the value is null then set params filter to `all`

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 import { AnimatePresence, LayoutGroup } from "framer-motion";
 
 import {
-  Badge,
+  CategoryButton,
   EmptyTodo,
   FormInputTodo,
+  ListTodos,
   ModalDialog,
-  TodoItem,
 } from "./components";
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filterTodoByCategoryValue = useMemo(() => {
+    return searchParams.get("filter");
+  }, [searchParams.get("filter")]);
+
   const {
     todo: { todos },
     modalDialog: { isOpen },
@@ -20,6 +26,14 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [categoryOnHover, setCategoryOnHover] = useState("null");
   const categories = ["All", "Active", "Completed"];
+
+  useEffect(() => {
+    if (searchParams.get("filter") === null) {
+      setSearchParams({
+        filter: "all",
+      });
+    }
+  }, []);
 
   return (
     <main className="mt-16 flex w-full flex-col items-center gap-8 overflow-auto px-12 pb-20 text-center sm:px-32 lg:px-44 xl:px-64 2xl:w-3/4 2xl:px-96">
@@ -38,7 +52,7 @@ function App() {
       >
         <LayoutGroup id="badges-group">
           {categories.map((category) => (
-            <Badge
+            <CategoryButton
               key={category}
               setSelectedCategory={setSelectedCategory}
               setCategoryOnHover={setCategoryOnHover}
@@ -46,7 +60,7 @@ function App() {
               isActive={selectedCategory === category}
             >
               {category}
-            </Badge>
+            </CategoryButton>
           ))}
         </LayoutGroup>
       </section>
@@ -54,10 +68,12 @@ function App() {
       <section className="mt-16 flex w-full flex-col gap-5 py-2">
         {todos.length < 1 && <EmptyTodo inputTodoRef={inputTodoRef} />}
         <AnimatePresence mode="sync">
-          {todos.length > 0 &&
-            todos.map((todo, index) => (
-              <TodoItem {...todo} index={index} key={todo.id} />
-            ))}
+          {todos.length > 0 && (
+            <ListTodos
+              todos={todos}
+              filterTodoByCategoryValue={filterTodoByCategoryValue}
+            />
+          )}
         </AnimatePresence>
       </section>
     </main>
